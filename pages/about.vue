c1a8f655055444f4a5f4f2ac19fae934008f872e
Alfred Lua <alfredlua@users.noreply.github.com>
2025-04-07 16:31:33 +0800

    Fix about top margin

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div class="h-screen overflow-hidden flex flex-col">
     <Navbar />
     <div class="w-full overflow-auto px-4">
-      <div class="prose prose-neutral my-36 mx-auto">
+      <div class="prose prose-neutral my-8 md:my-36 mx-auto">
         <ContentRenderer v-if="content" :value="content" />
       </div>
     </div>
