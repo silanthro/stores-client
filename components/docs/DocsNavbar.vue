3a73ef41b9502b1ea2f056931aa17e86d11f4720
Alfred Lua <alfredlua@users.noreply.github.com>
2025-04-07 16:12:19 +0800

    Make navbars responsive

--- components/docs/DocsNavbar.vue
+++ components/docs/DocsNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="z-10 w-full bg-white flex justify-between items-center py-1 px-10 border-b border-neutral-200">
-    <div class="flex items-center text-sm">
+    class="w-full bg-white flex justify-between items-center py-1 px-4 md:px-10 border-b border-neutral-200">
+    <div class="flex items-center text-sm overflow-scroll">
       <NuxtLink
         to="/docs"
         class="px-2 py-1 font-semibold text-xs uppercase font-display rounded bg-neutral-100">
