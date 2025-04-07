f78486c2ee390422cf2c9a1b04707e87d780a95f
Alfred Lua <alfredlua@users.noreply.github.com>
2025-04-07 16:23:29 +0800

    Make docs responsive

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full overflow-auto">
-    <div class="max-w-[72rem] mx-auto mt-8 mb-36 space-y-4">
+    <div class="max-w-[72rem] px-4 mx-auto mt-8 mb-36 space-y-4">
       <DocsInstallCard />
       <DocsQuickstartCard />
       <DocsGuideCard />
